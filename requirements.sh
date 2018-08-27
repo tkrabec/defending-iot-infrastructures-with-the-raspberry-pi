0f14f0627af8f4e1a8d63bdf8368b87b5d8d1374
Tkrabec <tkrabec@gmail.com>
2018-08-27 16:31:20 -0400

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+#This is not ready for a pull request
+#here is a list of missing modules I found when running this on a standard computer from source
+#tkrabec
+sudo pip install netaddr
+# python-tk package (pick you pakage manager)
+#sudo apt-get install python-tk
+#sudo yum install python-tk
